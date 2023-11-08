ec2a0352f57885dfbde2998c1ae4bac8c2db2da1
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-08 17:37:34 -0600

    Validando el presupuesto

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -1,10 +1,19 @@
 <script setup>
-    
+    import { ref } from 'vue';
+
+    const presupuesto = ref(0);
+
+    const definirPresupuesto = () => {
+        if(presupuesto.value <= 0){
+            console.log("Presupuesto no válido")
+        }
+    }
 </script>
 
 <template>
     <form
         class="presupuesto"
+        @submit.prevent="definirPresupuesto"
     >
         <div class="campo">
             <label for="nuevo-presupuesto">Definir Presupuesto</label>
@@ -14,6 +23,8 @@
                 class="nuevo-presupuesto"
                 placeholder="Añade tu presupuesto"
                 type="number"
+                :min="0"
+                v-model.number="presupuesto"
             >
         </div>
 
