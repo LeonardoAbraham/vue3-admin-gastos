35a78fe325f2872cd29ec15f6ba13f74206733ce
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-09 10:30:30 -0600

    Mostrando el Presupuesto y Disponible

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,5 +1,16 @@
 <script setup>
     import imagen from '../assets/img/grafico.jpg'
+
+    const props = defineProps({
+        presupuesto: {
+            type:Number,
+            required: true
+        },
+        disponible: {
+            type:Number,
+            required: true
+        }
+    })
 </script>
 
 <template>
@@ -19,13 +30,13 @@
                 <span>
                     Presupuesto:
                 </span>
-                $0
+                $ {{ presupuesto }}
             </p>
             <p>
                 <span>
                     Disponible:
                 </span>
-                $0
+                $ {{ disponible }}
             </p>
             <p>
                 <span>
