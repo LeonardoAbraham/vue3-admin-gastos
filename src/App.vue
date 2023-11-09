71a108a0c2c33a366766996d233b9ebab9046bb4
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-09 09:07:38 -0600

    Mostrando de forma condicional un componente si el presupuesto es válido

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
+    import { ref } from 'vue';
     import Presupuesto from './components/Presupuesto.vue';
+
+    const presupuesto = ref(0);
+
+    const definirPresupuesto = (cantidad) => {
+        presupuesto.value = cantidad
+    }
 
 </script>
 
@@ -8,7 +15,11 @@
         <header>
             <h1>Planificador de Gastos</h1>
             <div class="contenedor-header contenedor sombra">
-                <Presupuesto />
+                <Presupuesto 
+                    v-if="presupuesto === 0"
+                    @definir-presupuesto="definirPresupuesto"
+                />
+                <p v-else>Presupuesto valido</p>
             </div>
         </header>
     </div>
