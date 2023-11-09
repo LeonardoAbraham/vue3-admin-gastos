a3cc6152c71d6eb9d8867a3bc3a2f032f5b2198b
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-09 11:53:53 -0600

    Creando el componente para Modal

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-    import { ref } from 'vue';
+    import { ref, reactive } from 'vue';
     import Presupuesto from './components/Presupuesto.vue';
     import ControlPresupuesto from './components/ControlPresupuesto.vue';
-
+    import Modal from './components/Modal.vue'
     import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+
+    const modal = reactive({
+        mostrar: false,
+        animar: false
+    })
 
     const presupuesto = ref(0);
     const disponible = ref(0);
@@ -11,6 +16,11 @@
     const definirPresupuesto = (cantidad) => {
         presupuesto.value = cantidad;
         disponible.value = cantidad;
+    }
+
+    const mostrarModal = () => {
+        modal.mostrar = true;
+        modal.animar = true;
     }
 
 </script>
@@ -36,8 +46,12 @@
                 <img 
                     :src="iconoNuevoGasto" 
                     alt="icono nuevo gasto"
+                    @click="mostrarModal"
                 >
             </div>
+            <Modal 
+                v-if="modal.mostrar"
+            />
         </main>
     </div>
 </template>
