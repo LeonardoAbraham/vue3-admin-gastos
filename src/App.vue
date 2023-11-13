bd21e2756545b213b7ad9429d7636fc76d2c69db
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-11-12 18:27:31 -0600

    Generando un ID único sin dependencias

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     import Presupuesto from './components/Presupuesto.vue';
     import ControlPresupuesto from './components/ControlPresupuesto.vue';
     import Modal from './components/Modal.vue'
+    import { generarId } from './helpers'
     import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
     const modal = reactive({
@@ -46,7 +47,7 @@
     const guardarGasto = () => {
         gastos.value.push({
             ...gasto,
-            id: 123
+            id: generarId()
         })
     }
 
