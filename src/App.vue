e340208ec92de2b9995644dfadf31465b619824c
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-21 14:36:33 -0600

    Mostrando los gastos de la categoria seleccionada

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref, reactive, watch } from 'vue';
+    import { ref, reactive, watch, computed } from 'vue';
     import Presupuesto from './components/Presupuesto.vue';
     import ControlPresupuesto from './components/ControlPresupuesto.vue';
     import Modal from './components/Modal.vue'
@@ -111,6 +111,13 @@
         }
     }
 
+    const gastosFiltrados = computed (() => {
+        if(filtro.value){
+            return gastos.value.filter(gasto => gasto.categoria === filtro.value)
+        }
+        return gastos.value
+    })
+
 </script>
 
 <template>
@@ -135,10 +142,10 @@
                 v-model:filtro="filtro"
             />
             <div class="listador-gastos contenedor">
-                <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
+                <h2>{{ gastosFiltrados.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
 
                 <Gasto 
-                    v-for="gasto in gastos"
+                    v-for="gasto in gastosFiltrados"
                     :key="gasto.id"
                     :gasto="gasto"
                     @seleccionar-gasto="seleccionarGasto"
