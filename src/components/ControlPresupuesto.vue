ba5ea9416c87c6cef86c906d34ba47ffdaa8eb8f
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-21 15:30:33 -0600

    Mostrando un porcentaje de gastos

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { computed } from "vue";
     import "vue3-circle-progress/dist/circle-progress.css";
     import CircleProgress from "vue3-circle-progress";
     import { formatearCantidad } from '../helpers'
@@ -19,13 +20,18 @@
     })
 
     defineEmits(['reset-app'])
+
+    const porcentaje = computed(() => {
+        return parseInt(((props.presupuesto - props.disponible) / props.presupuesto ) * 100)
+    })
 </script>
 
 <template>
     <div class="dos-columnas">
         <div class="contenedor-grafico">
+            <p class="porcentaje">{{ porcentaje }}%</p>
             <CircleProgress 
-                :percent="50"
+                :percent="porcentaje"
                 :size="250"
                 :border-width="30"
                 :border-bg-width="30"
@@ -65,6 +71,21 @@
 </template>
 
 <style scoped>
+    .contenedor-grafico{
+        position: relative;
+    }
+    .porcentaje {
+        position: absolute;
+        margin: auto;
+        top: calc(50% - 1.5rem);
+        left: 0;
+        right: 0;
+        text-align: center;
+        z-index: 100;
+        font-size: 3rem;
+        font-weight: 900;
+        color: var(--gris-oscuro)
+    }
     .dos-columnas {
         display: flex;
         flex-direction: column;
